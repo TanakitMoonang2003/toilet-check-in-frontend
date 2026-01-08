bc9caf4d6d51f63e29108b596892716c8b8e878d
tanakit <tanakit2003moonang@gmail.com>
2026-01-08 17:30:07 +0700

    UPDATE

--- next.config.js
+++ next.config.js
@@ -8,12 +8,11 @@ const nextConfig = {
       },
     ];
   },
-  env: {
-    NEXT_PUBLIC_URL: 'http://localhost:3000/',
-    NEXT_PUBLIC_BACK_END: 'http://localhost:5000/',
-    NEXT_PUBLIC_MAPBOX_TOKEN: 'pk.eyJ1IjoibmFtZXZveTEyMyIsImEiOiJjbTkyemt2cmowYWM5MndzNDZ5eDN2d3B1In0.v7kTe6KFT0ikRTqYngqQyg'
-  },
-
+  // env: {
+  //   NEXT_PUBLIC_URL: 'http://localhost:3000/',
+  //   NEXT_PUBLIC_BACK_END: 'http://localhost:5000/',
+  //   NEXT_PUBLIC_MAPBOX_TOKEN: 'pk.eyJ1IjoibmFtZXZveTEyMyIsImEiOiJjbTkyemt2cmowYWM5MndzNDZ5eDN2d3B1In0.v7kTe6KFT0ikRTqYngqQyg'
+  // },
   images: {
     remotePatterns: [
       {
